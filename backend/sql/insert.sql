ed778d4533e5eeb46c4e9fa8d1cc9e7961fd75bb
Jasmine Skarpfors <jasmine@Jasmines-MacBook-Pro.local>
2021-03-18 22:32:39 +0200

    added images and sql

--- backend/sql/insert.sql
+++ backend/sql/insert.sql
@@ -1,18 +1,32 @@
 
 USE diy_community;
 
-INSERT INTO `diy_events` (`title`, `description`, `date`, `image`, `category`) 
-VALUES
+INSERT INTO `diy_events` (`id`, `title`, `category`, `description`, `date`, `image`) VALUES
 
-('Swedish Tapas', 'A Fun day eating Tapas', '2021-04-05', 'tapas.jpg', 'food'),
-('Veggie Pokebowl', 'My favourite recipes for a veggie pokebowl', '2021-04-08', 'food.jpg', 'food'),
-('Leather Treatment', 'Learn to take care of your leather products', '2021-04-10', 'leather.jpg', 'sewing'),
-('Knitting', 'Learn how to knit!', '2021-04-04', 'sewing.jpg', 'sewing'),
-('Sewing blankets', 'Sew blankets for homeless dogs', '2021-04-05', '1.jpg', 'sewing'),
-('Easy table tips!', 'Quick tips on how to set a table', '2021-04-02', 'decor.jpg', 'craft'),
-('HOMEMADE HANDSOAP', 'Make your own soap', '2021-04-01', 'handsoap.jpg', 'beauty'),
-('LAVENDEL OIL', 'How to use Lavender oil in multiple ways', '2021-10-01', 'oil.jpg', 'beauty'),
-('Pizza night!', 'Join me for pizza night!', '2021-05-01', '', 'food'),
-('HOMEMADE HERBAL TEA', 'Make your own tea with things from your garden', '2021-06-10', 'tea.jpg', 'food'),
-('GRAPEFRUIT SCRUB', 'My favourite scrub recipe', '2021-06-10', 'grapescrub.jpg', 'beauty'),
-('HOMEMADE BODYLOTION', 'easy homemade bodylotion', '2021-05-07', 'lotion.jpg', 'beauty')
+('Burger night', 'food', 'make burgers with me', '2021-08-09 00:00:00', 'burgers.jpg'),
+('Creative painting', 'art', 'get Creative while painting', '2021-06-16 00:00:00', '3.jpg'),
+('Easy table tips!', 'craft', 'Quick tips on how to set a table', '2021-04-02 00:00:00', 'decor.jpg'),
+('Fish and chips', 'food', 'homemade fish and chips', '2021-05-02 00:00:00', 'remoulade.jpg'),
+('GRAPEFRUIT SCRUB', 'beauty', 'My favourite scrub recipe', '2021-06-10 00:00:00', 'grapescrub.jpg'),
+('Green smoothies', 'food', 'healthy green smoothies', '2021-06-04 00:00:00', 'smoothie.jpg'),
+('HOMEMADE BODYLOTION', 'beauty', 'easy homemade bodylotion', '2021-05-07 00:00:00', 'lotion.jpg'),
+('HOMEMADE HANDSOAP', 'beauty', 'Make your own soap', '2021-04-01 00:00:00', 'handsoap.jpg'),
+('HOMEMADE HERBAL TEA', 'food', 'Make your own tea with things from your garden', '2021-06-10 00:00:00', 'tea.jpg'),
+('HOMEMADE pasta course', 'food', 'who doesnt love pasta?', '2021-06-07 00:00:00', 'pasta.jpg'),
+('Hotdog', 'food', 'lets meet to make hotdogs', '2021-09-07 00:00:00', 'pizza.jpg'),
+('Knitting', 'sewing', 'Learn how to knit!', '2021-04-04 00:00:00', 'sewing.jpg'),
+('LAVENDEL OIL', 'beauty', 'How to use Lavender oil in multiple ways', '2021-10-01 00:00:00', 'oil.jpg'),
+('Leather Treatment', 'sewing', 'Learn to take care of your leather products', '2021-04-10 00:00:00', 'leather.jpg'),
+('paint with me', 'beauty', 'Paint with me', '2021-04-08 00:00:00', '1.jpg'),
+('Pizza night!', 'food', 'Join me for pizza night!', '2021-05-01 00:00:00', 'hotdog.jpg'),
+('Secret Seafood Pasta', 'food', 'my favourite dish of all time', '2021-04-07 00:00:00', 'pasta2.jpg'),
+('Sewing blankets', 'sewing', 'Sew blankets for homeless dogs', '2021-04-05 00:00:00', '1.jpg'),
+('Sewing blankets for dogs', 'craft', 'Sew blankets for homeless dogs with me', '2021-04-05 16:16:48', 'advanced.jpg'),
+('Soup for charity', 'food', 'Soup for the soup kitchen', '2021-05-09 00:00:00', 'soup.jpg'),
+('Swedish Tapas', 'food', 'A Fun day eating Tapas', '2021-04-05 00:00:00', 'tapas.jpg'),
+('Swedish Tapas', 'food', 'A Fun day eating Tapas', '2021-04-05 00:00:00', 'tapas.jpg'),
+('Taco tuesday', 'food', 'my favourite mexican crousine', '2021-06-19 00:00:00', 'taco.jpg'),
+('The C in Salad', 'food', 'Variations of Ceasar Salads', '2022-01-02 00:00:00', 'ceasar.jpg'),
+('Veggie Pokebowl', 'food', 'My favourite recipes for a veggie pokebowl', '2021-04-08 00:00:00', 'food.jpg'),
+('Veggie Pokebowl', 'food', 'My favourite recipes for a veggie pokebowl', '2021-04-08 00:00:00', 'food.jpg'),
+('vietnamese spring rolls', 'food', 'my moms seret recipe', '2021-04-07 00:00:00', 'springroll.jpg');
